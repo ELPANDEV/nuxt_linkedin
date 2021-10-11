fd3f8670aaaf14171b89c3d295e7a4ed4dc849b3
Francisco Moncayo <javoavmystery@gmail.com>
2021-10-11 10:29:14 -0500

    8ff4c99

---
+++ _nuxt/2.8f1ac2fa72f33c05057e.js
@@ -0,0 +1,1 @@
+(window.webpackJsonp=window.webpackJsonp||[]).push([[2],{308:function(t,e,n){"use strict";n.r(e);var l=n(0).a.extend({}),c=n(2),component=Object(c.a)(l,(function(){var t=this,e=t.$createElement;return(t._self._c||e)("div",{staticClass:"v-body"},[t._t("default")],2)}),[],!1,null,null,null);e.default=component.exports}}]);
